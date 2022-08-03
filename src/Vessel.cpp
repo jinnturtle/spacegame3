fa4739d0e5b21044117c78d9ee1bee7585182ed8
jinnturtle <egined@gmail.com>
2022-08-03 12:53:09 +0100

    fixed math error in nav

--- src/Vessel.cpp
+++ src/Vessel.cpp
@@ -10,7 +10,7 @@ Vessel::Vessel()
 Vessel::Vessel(Coord2d pos, Coord2d v)
     : pos {pos}
     , v {v}
-    , nav_params {.mode = NAV_idle, .max_acc = 1.0, .target = nullptr}
+    , nav_params {.mode = NAV_idle, .max_acc = 0.1, .target = nullptr}
 {}
 
 auto Vessel::get_pos() -> Coord2d
@@ -55,40 +55,28 @@ auto Vessel::nav_approach() -> void
 
     double abs_dist_x = abs(dist_x);
     double abs_dist_y = abs(dist_y);
-    double margin_x {0.0};
-    double margin_y {0.0};
-    if (abs_dist_x < abs_dist_y) {
-        margin_x = abs_dist_x / abs_dist_y;
-        margin_y = 1.0 - margin_y;
-    } else {
-        margin_y = abs_dist_y / abs_dist_x;
-        margin_x = 1.0 - margin_x;
-    }
+
+    double dir_x {abs_dist_x / (abs_dist_x + abs_dist_y)};
+    if (dir_x > 1.0) { dir_x = 1.0 / dir_x; }
+    double dir_y {1.0 - dir_x};
+    if (dist_x < 0.0) { dir_x = -dir_x; }
+    if (dist_y < 0.0) { dir_y = -dir_y; }
 
     double acc {this->nav_params.max_acc};
-    double acc_x = acc * margin_x;
-    double acc_y = acc * margin_y;
+    double acc_x = acc * dir_x;
+    double acc_y = acc * dir_y;
 
     double total_velocity {abs(this->v.x) + abs(this->v.y)};
     double braking_time {total_velocity / this->nav_params.max_acc};
-    double braking_dist {total_velocity * braking_time / 2};
+    double braking_dist {total_velocity * braking_time};
     double distance_to_tgt = abs_dist_x + abs_dist_y;
 
     DBG(1, "distance to target: ", distance_to_tgt);
     DBG(1, "braking distance: ", braking_dist);
-    DBG(1, "acceleration: ", acc);
+    DBG(1, "acceleration: x/y ", acc_x, "/", acc_y);
 
-
-    if (dist_x > 0.0) {
-        this->v.x += acc_x;
-    } else {
-        this->v.x -= acc_x;
-    }
-    if (dist_y > 0.0) {
-        this->v.y += acc_y;
-    } else {
-        this->v.y -= acc_y;
-    }
+    this->v.x += acc_x;
+    this->v.y += acc_y;
 }
 
 
